9a50c0e269d2548bd0292a461cd7d6814a5fa91d
Alexander <kava3331@mail.ru>
2021-08-09 13:45:21 +0300

    Init commit

---
+++ components/CardList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Card List
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Tutorial />
+    <div class="test">
+      Some text
+      <div class="inner">
+        fffffffffffffffffffffffffff
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.test {
+  background-color: red;
+  .inner {
+    background-color: yellow;
+  }
+}
+</style>
