b934c95725bc0736ac55ae9f36b377585f033263
Alexander <kava3331@mail.ru>
2021-08-23 02:36:02 +0300

    Начало реализации контент лоадера и анимаций

--- components/Card.vue
+++ components/Card.vue
@@ -1,23 +1,51 @@
 <template>
   <div class="card">
-    <div class="card-img">
-      <img :src="product.imgUrl" alt="Продукт" />
-    </div>
-    <div class="card-text">
-      <div class="card-title">{{ product.name }}</div>
-      <div class="card-description">
-        {{ product.description }}
+    <content-loader v-if="!isLoading" :width="400" :height="540" :speed="2" primaryColor="#f3f3f3" secondaryColor="#cccccc" uniqueKey="1">
+      <rect x="0" y="0" rx="0" ry="0" width="400" height="242" />
+      <rect x="16" y="259" rx="0" ry="0" width="243" height="31"></rect>
+      <rect x="16" y="310" rx="0" ry="0" width="362" height="130"></rect>
+      <rect x="16" y="465" rx="0" ry="0" width="170" height="37"></rect>
+    </content-loader>
+    <template v-else>
+      <div class="card-img">
+        <img :src="isImgUrlValid ? product.imgUrl : '/img/default-img.jpg'" alt="Продукт" />
       </div>
-      <div class="card-price">{{ product.price }} руб.</div>
-    </div>
-    <div class="card-delete" @click="removeProductHandler(product.id)"></div>
+      <div class="card-text">
+        <div class="card-title">{{ product.name }}</div>
+        <div class="card-description">
+          {{ product.description }}
+        </div>
+        <div class="card-price">{{ product.price }} руб.</div>
+      </div>
+      <div class="card-delete" @click="removeProductHandler(product.id)"></div>
+    </template>
   </div>
 </template>
 
 <script>
+import { ContentLoader } from "vue-content-loader";
+
 export default {
+  components: {
+    ContentLoader
+  },
   props: {
     product: Object
+  },
+  data() {
+    return {
+      isLoading: false
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.isLoading = true;
+    }, 2000);
+  },
+  computed: {
+    isImgUrlValid() {
+      return /^https?:\/\/.*\.(?:jpe?g|gif|png)$/gi.test(this.product.imgUrl);
+    }
   },
   methods: {
     removeProductHandler(productIdForDelete) {
@@ -40,6 +68,7 @@ export default {
   border-radius: 4px;
   margin-right: 16px;
   margin-bottom: 16px;
+  transition: box-shadow 0.25s ease;
   @media screen and (max-width: $max-width-laptop) {
     width: calc((100% - 32px) / 2);
   }
@@ -49,10 +78,22 @@ export default {
   @media screen and (max-width: $max-width-mobile) {
     margin-right: 0;
   }
+  &:hover {
+    box-shadow: rgb(0 0 0 / 10%) 0px 20px 55px, rgb(0 0 0 / 12%) 0px -12px 30px, rgb(0 0 0 / 12%) 0px 4px 6px, rgb(0 0 0 / 3%) 0px 12px 13px,
+      rgb(0 0 0 / 9%) 0px -3px 5px;
+    .card-delete {
+      transform: scale(1);
+    }
+  }
   .card-img {
     text-align: center;
     margin-bottom: 16px;
     height: 200px;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
   }
   .card-text {
     flex-grow: 1;
@@ -93,6 +134,8 @@ export default {
     box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
     border-radius: 10px;
     cursor: pointer;
+    transform: scale(0);
+    transition: transform 0.25s ease;
   }
 }
 </style>
--- components/CardList.vue
+++ components/CardList.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="cards-list">
+  <transition-group class="cards-list" name="cards-list" tag="div">
     <template v-for="(item, index) in productsList">
       <Card :product="item" @remove-product="removeProduct" :key="item.id"></Card>
     </template>
-  </div>
+  </transition-group>
 </template>
 
 <script>
@@ -28,7 +28,35 @@ export default {
 .cards-list {
   display: flex;
   flex-wrap: wrap;
+  position: relative;
   width: calc(100% - 332px - 16px + 16px);
   margin-right: -16px;
+  &.loading {
+    *::before {
+      content: "";
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      background: hsla(0, 0%, 100%, 0.5);
+      z-index: 100;
+    }
+  }
+}
+
+.cards-list-enter-active,
+.cards-list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.cards-list-enter-from,
+.cards-list-leave-to {
+  opacity: 0;
+  transform: translateY(10px);
+}
+
+.cards-list-move {
+  transition: transform 0.8s ease;
 }
 </style>
